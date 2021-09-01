3a1b0945ac0e5a99f9eb5c859daa3fa7a059c37b
turyabiswas233 <78296735+turyabiswas233@users.noreply.github.com>
2021-09-01 17:59:26 +0600

    Update price_script.js

--- price_script.js
+++ price_script.js
@@ -5,61 +5,63 @@ let i = 1,
   j = 1;
 
 //prices
-let price1 = 54.99;
-let price2 = 74.99;
-let totalPrice = price1 + price2 + 19;
-total.innerHTML = totalPrice;
+let price1 = 54.99*100;
+let price2 = 74.99*100;
+const firstPrc = 5499;
+const secondPrc = 7499;
+let totalPrice = price1 + price2 + 19*100;
+total.innerHTML = totalPrice/100;
 
 //first item's button set
 function plus_qnt1() {
   i++;
   firstQnt.innerHTML = i;
-  price1 += 54.99;
-  totalPrice += 54.99;
-  total.innerHTML = totalPrice;
+  price1 += firstPrc;
+  totalPrice += firstPrc;
+  total.innerHTML = totalPrice/100;
 }
 function min_qnt1() {
   i--;
   if (i < 1) {
     i = 1;
   } else {
-    price1 -= 54.99;
-    if (price1 < 54.99) {
-      price1 = 54.99;
+    price1 -= firstPrc;
+    if (price1 < firstPrc) {
+      price1 = firstPrc;
     }
-    totalPrice -= 54.99;
+    totalPrice -= firstPrc;
     if (totalPrice < 148.98) {
       totalPrice = 148.98;
     }
   }
   firstQnt.innerHTML = i;
-  total.innerHTML = totalPrice;
+  total.innerHTML = totalPrice/100;
 }
 
 //second item's  button set
 function plus_qnt2() {
   j++;
   secondQnt.innerHTML = j;
-  price2 += 74.99;
-  totalPrice += 74.99;
-  total.innerHTML = totalPrice;
+  price2 += secondPrc;
+  totalPrice += secondPrc;
+  total.innerHTML = totalPrice/100;
 }
 function min_qnt2() {
   j--;
   if (j < 1) {
     j = 1;
   } else {
-    price2 -= 74.99;
-    if (price2 < 74.99) {
-      price2 = 74.99;
+    price2 -= secondPrc;
+    if (price2 < secondPrc) {
+      price2 = secondPrc;
     }
-    totalPrice -= 74.99;
+    totalPrice -= secondPrc;
     if (totalPrice < 148.98) {
       totalPrice = 148.98;
     }
   }
   secondQnt.innerHTML = j;
-  total.innerHTML = totalPrice;
+  total.innerHTML = totalPrice/100;
 }
 // for (let num = 0; num < 1000; num++) {
 //   console.log(num);
